46932ee13748e1515907e03478ca53f4aa048564
Ehmad <justEhmadSaeed@gmail.com>
2020-07-14 12:04:40 +0500

    👌 IMPROVE: ADD teacher, class relationship

--- pseudocode/implementation/index.js
+++ pseudocode/implementation/index.js
@@ -35,22 +35,64 @@ const period = {
   p: 7,
 };
 
-const c = ["c1", "c2", "c3", "c4", "c5", "c6"];
+const c = ["c1", "c2", "c3"];
 const s = [
   { name: "s1", creditHr: 3 },
   { name: "s2", creditHr: 3 },
   { name: "s3", creditHr: 3 },
   { name: "s4", creditHr: 3 },
   { name: "s5", creditHr: 3 },
-  { name: "s6", creditHr: 3 },
+  { name: "s6", creditHr: 2 },
 ];
 const t = [
-  { name: "t1", subject: s[0], lecture: [2, 1] },
-  { name: "t2", subject: s[1], lecture: [1, 1, 1] },
-  { name: "t3", subject: s[2], lecture: [1, 1, 1] },
-  { name: "t4", subject: s[3], lecture: [1, 1, 1] },
-  { name: "t5", subject: s[4], lecture: [1, 1, 1] },
-  { name: "t6", subject: s[5], lecture: [3] },
+  {
+    name: "t0",
+    assigned: [
+      { class: c[0], subject: s[0], lecture: [1, 1, 1] },
+      { class: c[1], subject: s[0], lecture: [1, 1, 1] },
+      { class: c[2], subject: s[0], lecture: [1, 1, 1] },
+    ],
+  },
+  {
+    name: "t1",
+    assigned: [
+      { class: c[0], subject: s[1], lecture: [1, 1, 1] },
+      { class: c[1], subject: s[1], lecture: [1, 1, 1] },
+      { class: c[2], subject: s[1], lecture: [1, 1, 1] },
+    ],
+  },
+  {
+    name: "t2",
+    assigned: [
+      { class: c[0], subject: s[2], lecture: [2, 1] },
+      { class: c[1], subject: s[2], lecture: [2, 1] },
+      { class: c[2], subject: s[2], lecture: [2, 1] },
+    ],
+  },
+  {
+    name: "t3",
+    assigned: [
+      { class: c[0], subject: s[3], lecture: [1, 1, 1] },
+      { class: c[1], subject: s[3], lecture: [1, 1, 1] },
+      { class: c[2], subject: s[3], lecture: [1, 1, 1] },
+    ],
+  },
+  {
+    name: "t4",
+    assigned: [
+      { class: c[0], subject: s[4], lecture: [1, 1, 1] },
+      { class: c[1], subject: s[4], lecture: [1, 1, 1] },
+      { class: c[2], subject: s[4], lecture: [1, 1, 1] },
+    ],
+  },
+  {
+    name: "t5",
+    assigned: [
+      { class: c[0], subject: s[5], lecture: [1, 1] },
+      { class: c[1], subject: s[5], lecture: [2] },
+      { class: c[2], subject: s[5], lecture: [2] },
+    ],
+  },
 ];
 const teacherSubjectHrs = ["s1", "s2", "s3", "s4", "s5"];
 
@@ -63,23 +105,31 @@ const c_available = ThreeDarray(c.length, period.d, period.p);
 const remainingLectures = [];
 for (let i = 0; i < c.length; i++) {
   remainingLectures[i] = [];
-  for (let j = 0; j < t.length; j++)
-    remainingLectures[i][j] = t[j].subject.creditHr;
+  for (let j = 0; j < t.length; j++) {
+    let valid = t[j].assigned.findIndex((e) => e.class === c[i]);
+    remainingLectures[i][j] =
+      valid !== -1 ? t[j].assigned[valid].subject.creditHr : 0;
+  }
 }
+console.table(remainingLectures);
+
 const Scheduling = () => {
   for (let per = 0; per < period.p; per++) {
-    c.forEach((clas, cIndex) => {
-      for (let day = 0; day < period.d; day++) {
+    for (let day = 0; day < period.d; day++) {
+      c.forEach((clas, cIndex) => {
         t.forEach((teacher, tIndex) => {
+          let valid = teacher.assigned.findIndex((e) => e.class === clas);
           if (
-            t_available[tIndex][day].find((e) => e === clas) ||
+            valid === -1 ||
+            t_available[tIndex][day].some((e) => e === clas) ||
             remainingLectures[cIndex][tIndex] === 0
-          )
+          ) {
             return;
+          }
 
           if (isSchedulePossible(tIndex, cIndex, { d: day, p: per })) {
             let lectureCount = 1;
-            let longestLecture = t[tIndex].lecture[0];
+            let longestLecture = teacher.assigned[valid].lecture[0];
             if (
               remainingLectures[cIndex][tIndex] > 1 &&
               longestLecture > 1 &&
@@ -94,21 +144,31 @@ const Scheduling = () => {
             }
             for (let i = 0; i < lectureCount; ++i) {
               final_tt[cIndex][day][per + i] = teacher.name;
-              c_available[cIndex][day][per + i] = teacher;
+              c_available[cIndex][day][per + i] = teacher.name;
               t_available[tIndex][day][per + i] = clas;
               remainingLectures[cIndex][tIndex]--;
             }
           }
         });
-      }
-    });
+      });
+    }
   }
-
-  console.table(remainingLectures);
-  final_tt.forEach((tt, i) => {
-    console.log("Class: ", i + 1);
-
-    console.table(tt);
-  });
 };
 Scheduling();
+final_tt.forEach((tt, i) => {
+  console.log("Class: ", i + 1);
+
+  console.table(tt);
+});
+
+let remaining = [];
+remainingLectures.forEach((lecture, i) => {
+  lecture.forEach((value, j) => {
+    if (value > 0) {
+      remaining.push({ Class: i, teacher: j });
+    }
+  });
+});
+console.log("Remaining Lectures: " + remaining);
+
+console.table(remainingLectures);

f2aefd32effd937d85ac2f90c6ab779763758f03
Ehmad <justEhmadSaeed@gmail.com>
2020-08-04 23:47:40 +0500

    👌 IMPROVE: UPDATE packages

--- src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
-export const PASSWORD_FORGET = '/pw-forget';
